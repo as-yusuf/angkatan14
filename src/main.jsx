ce547508fc8b871822d5c4598adc25685b00f53e
as-yusuf <akhmadrahadi28@guru.smk.belajar.id>
2025-04-17 10:47:11 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 // src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "App.jsx";
+import App from "./App.jsx";
 import "./index.css"; // opsional, kalau kamu pakai Tailwind atau CSS sendiri
 
 ReactDOM.createRoot(document.getElementById("root")).render(
